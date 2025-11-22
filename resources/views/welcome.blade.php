fa066e4e47ef69e267e9147b8e243ab982bdba2e
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2025-11-22 16:46:15 -0600

    feat: actualizar descripción en la página de bienvenida para reflejar mejor las herramientas disponibles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
     </div>
     <div class="mx-auto mt-8 max-w-2xl text-center">
         <h2 class="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl dark:text-white"><span class="animated-gradient text-5xl">Achronyme</span><br>Herramientas para Ingeniería</h2>
-        <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">Una suite de herramientas web para el análisis y visualización de conceptos clave del Procesamiento Digital de Señales (PDS), incluyendo la Transformada de Fourier, Series de Fourier, Convolución y más.</p>
+        <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">Colección modular de utilidades web para el análisis de sistemas y resolución de problemas técnicos. Centralizamos herramientas de PDS, Inteligencia Artificial, Optimización y Ciencias de la Computación en una sola plataforma de código abierto.</p>
     </div>
     <svg aria-hidden="true" class="absolute inset-0 -z-10 size-full mask-[radial-gradient(100%_100%_at_top_right,white,transparent)] stroke-gray-200 dark:stroke-white/10">
         <defs>
